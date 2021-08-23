807f779aab9b761c361d44312b3615beca1f6f62
Hiltunen Matias <matias.hiltunen@lapinamk.fi>
2021-08-23 16:40:06 +0300

    add serving the userscript on localhost

--- build.js
+++ build.js
@@ -1,23 +1,30 @@
-const fs = require('fs')
+import fs from 'fs'
+import { buildSync, serve } from 'esbuild'
 
-const monkeyHeader = `// ==UserScript==
-// @author         Matias Hiltunen
+
+const author = "Matias Hiltunen"
+const version = "0.0.1"
+const updateUrl = "http://127.0.0.1:8080/total-conversion-build.meta.js"
+const downloadUrl = "http://127.0.0.1:8080/total-conversion-build.user.js"
+
+const monkeyMeta = `// ==UserScript==
+// @author         ${author}
 // @name           IITC: Ingress intel map total conversion - TypeScript Edition
-// @version        0.0.1
+// @version        ${version}
 // @description    Total conversion for the ingress intel map.
 // @run-at         document-end
-// @id             total-conversion-build
+// @id             total-conversion-build-ts
 // @namespace      https://github.com/MatiasHiltunen/IITC-TypeScript-Edition
+// @updateURL      ${updateUrl}
+// @downloadURL    ${downloadUrl}
 // @match          https://intel.ingress.com/*
 // @grant          none
-// ==/UserScript==
+// ==/UserScript==`
 
+const monkeyHeader = `${monkeyMeta}
 function wrapper(plugin_info) {
 // ensure plugin framework is there, even if iitc is not yet loaded
-
 if (typeof window.plugin !== 'function') window.plugin = function() {};
-
-
 window.script_info = plugin_info;
 `
 
@@ -30,21 +37,45 @@ if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = {
 script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(info) + ');'));
 (document.body || document.head || document.documentElement).appendChild(script);`
 
-require('esbuild').build({
-        entryPoints: [
-            // './core/code/boot.ts',
-            './core/total-conversion-build.ts'
-        ],
-        bundle: true,
-        minify: true,
-        sourcemap: false,
-        target: 'es6',
-        loader: { '.png': 'dataurl' },
-        // outfile: './core/code/boot.js',
-        outfile: './core/total-conversion-build.js'
-    }).then((_) => {
-        let fileContent = monkeyHeader + fs.readFileSync('./core/total-conversion-build.js', 'utf-8') + monkeyBottom
 
-        fs.writeFileSync('./core/total-conversion-build.js', fileContent)
-    })
-    .catch(() => process.exit(1))
+const outfile = 'total-conversion-build.js'
+const userScriptPath = "total-conversion-build.user.js"
+const userMetaPath = "total-conversion-build.meta.js"
+
+const buildConfig = {
+    entryPoints: ['./core/total-conversion-build.ts'],
+    bundle: true,
+    minify: true,
+    sourcemap: false,
+    target: 'es6',
+    loader: { '.png': 'dataurl' },
+    outdir: './build'
+}
+
+const buildMonkeyScript = () => {
+    const transpiledTsFile = buildConfig.outdir + '/' + outfile
+
+    let fileContent = monkeyHeader + fs.readFileSync(transpiledTsFile, 'utf-8') + monkeyBottom
+
+    if (fs.readdirSync(buildConfig.outdir).includes(userScriptPath)) fs.rmSync(buildConfig.outdir + '/' + userScriptPath)
+    fs.writeFileSync(buildConfig.outdir + '/' + userScriptPath, fileContent)
+
+    if (fs.readdirSync(buildConfig.outdir).includes(userMetaPath)) fs.rmSync(buildConfig.outdir + '/' + userMetaPath)
+    fs.writeFileSync(buildConfig.outdir + '/' + userMetaPath, monkeyMeta)
+}
+
+buildSync(buildConfig)
+buildMonkeyScript()
+
+serve({
+    servedir: buildConfig.outdir,
+    port: 8080,
+    host: "localhost",
+}, {}).then((server) => {
+
+    let { host, port } = server;
+
+    console.log(
+        `Development server is running on http://${host}:${port}`
+    );
+});
--- core/total-conversion-build.ts
+++ core/total-conversion-build.ts
@@ -5,19 +5,6 @@ import { Player } from "./code/player";
 import { RegionScoreboard } from "./code/region_scoreboard";
 import { store } from "./code/store";
 
-// ==UserScript==
-// @author         Matias Hiltunen
-// @name           IITC: Ingress intel map total conversion - TypeScript Edition
-// @version        0.0.1
-// @description    Total conversion for the ingress intel map.
-// @run-at         document-end
-// @id             total-conversion-build
-// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
-// @updateURL      https://iitc.app/build/release/total-conversion-build.meta.js
-// @downloadURL    https://iitc.app/build/release/total-conversion-build.user.js
-// @match          https://intel.ingress.com/*
-// @grant          none
-// ==/UserScript==
 
 
 
